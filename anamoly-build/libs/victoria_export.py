1c71ece93c0a1685cd8cf1155bc30f0ff674f089
satwik81 <satwik89@gmail.com>
2025-07-24 01:39:30 +0530

    remove headers in url

--- anamoly-build/libs/victoria_export.py
+++ anamoly-build/libs/victoria_export.py
@@ -7,9 +7,8 @@ def export_to_victoriametrics(rule, value, exports):
     try:
         # Prepare the time-series data in the Prometheus format
         data = f'{rule["name"]} {value} {int(datetime.utcnow().timestamp())}\n'
-        headers = {'Content-Type': 'text/plain'}
         # Send data to the /api/v1/import endpoint
-        response = requests.post(f'{vm_config["url"]}/api/v1/import', data=data, headers=headers)
+        response = requests.post(f'{vm_config["url"]}/api/v1/import', data=data)
         
         if response.status_code == 200:
             print(f"Exported to VictoriaMetrics: {rule['name']} - value: {value}")
