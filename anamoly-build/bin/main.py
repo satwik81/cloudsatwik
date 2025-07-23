8ec3c19b6d69e516d5f7be7342c290a2621c009d
satwik81 <satwik89@gmail.com>
2025-07-23 23:53:45 +0530

    add python path

--- anamoly-build/bin/main.py
+++ anamoly-build/bin/main.py
@@ -2,7 +2,10 @@ import sys
 import os
 
 # Add the libs folder to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the full path to the libs directory
+sys.path.append(os.path.join(project_root, 'libs'))
 from libs.config_loader import load_config
 from libs.prometheus_query import query_prometheus
 from libs.mysql_exporter import export_to_mysql
