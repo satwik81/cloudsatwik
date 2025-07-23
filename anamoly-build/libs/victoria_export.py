8cef230de45c92397c968ae940257a2022011c21
satwik81 <satwik89@gmail.com>
2025-07-24 01:06:10 +0530

    fix url path

--- anamoly-build/libs/victoria_export.py
+++ anamoly-build/libs/victoria_export.py
@@ -9,7 +9,7 @@ def export_to_victoriametrics(rule, value, exports):
         data = f'{rule["name"]} {value} {int(datetime.utcnow().timestamp())}\n'
         
         # Send data to the /api/v1/import endpoint
-        response = requests.post(f'{vm_config["url"]}/insert/0/prometheus/api/v1/import', data=data)
+        response = requests.post(f'{vm_config["url"]}/insert/0/prometheus/api/v1/import/prometheus', data=data)
         
         if response.status_code == 200:
             print(f"Exported to VictoriaMetrics: {rule['name']} - value: {value}")
