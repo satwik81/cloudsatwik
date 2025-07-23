a8735a104e55f5ff135cc58eef6444b9e6304658
satwik81 <satwik89@gmail.com>
2025-07-24 01:16:15 +0530

    fix url with headers

--- anamoly-build/libs/victoria_export.py
+++ anamoly-build/libs/victoria_export.py
@@ -7,9 +7,9 @@ def export_to_victoriametrics(rule, value, exports):
     try:
         # Prepare the time-series data in the Prometheus format
         data = f'{rule["name"]} {value} {int(datetime.utcnow().timestamp())}\n'
-        
+        headers = {'Content-Type': 'text/plain'}
         # Send data to the /api/v1/import endpoint
-        response = requests.post(f'{vm_config["url"]}/insert/0/prometheus/api/v1/import/prometheus', data=data)
+        response = requests.post(f'{vm_config["url"]}/insert/0/prometheus/api/v1/import', data=data, headers=headers)
         
         if response.status_code == 200:
             print(f"Exported to VictoriaMetrics: {rule['name']} - value: {value}")
